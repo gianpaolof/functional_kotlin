92314f0f9dc88b03da2f26b3af1dfd1bac6ab128
gianpaolo <gianpaolof@gmail.com>
2024-09-17 19:12:22 +0200

    a silly test

---
+++ src/test/kotlin/HelloFunctionalTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+
+fun sumAndLog(a: Int, b: Int, log: (Int) -> Unit): Int = (a + b).also(log)
+class HelloTest{
+    @Test
+    fun atest(): Unit {
+        var result = sumAndLog(1,5) { print("suka") }
+
+        println(result)
+        result = sumAndLog(5, 3) { sum ->
+            println("The sum is: $sum")
+        }
+        println(result)
+    }
+}
